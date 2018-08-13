f7f31cc8b40a5e7fd1a5cebae3c750924a6da003
MarkNewcomb1 <wlmmn@hotmail.com>
2018-08-13 16:13:34 -0600

    added cors to index.js

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 var express = require("express"),
   app = express(),
   port = process.env.PORT || 8000,
-  bodyParser = require("body-parser");
+  bodyParser = require("body-parser"),
+  cors = require("cors");
 
 var videoRoutes = require("./routes/videos");
 
 app.use(bodyParser.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cors());
 
 app.use("/api/videos", videoRoutes);
 
