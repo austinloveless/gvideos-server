8f9391932074016cdc6f3266a3b1b9fcca59705c
austinloveless <austinloveless5171@gmail.com>
2018-08-13 15:22:57 -0600

    cleaned up index.js

--- index.js
+++ index.js
@@ -7,8 +7,6 @@ var videoRoutes = require("./routes/videos");
 
 app.use(bodyParser.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static(__dirname + "/public"));
-app.use(express.static(__dirname + "/views"));
 
 app.use("/api/videos", videoRoutes);
 
